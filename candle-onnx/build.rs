9d2db428efbde7130179c6044359f194c4181f52
Aster <aster@vers.site>
2024-04-21 21:30:11 +0800

    Remove protoc from CI

--- candle-onnx/build.rs
+++ candle-onnx/build.rs
@@ -1,5 +1,5 @@
 fn main() -> anyhow::Result<()> {
     let fds = protox::compile(&["src/onnx.proto3"], &["src/"])?;
-    prost_build::compile_fds(fds)?;
+     prost_build::compile_fds(fds)?;
     Ok(())
 }
